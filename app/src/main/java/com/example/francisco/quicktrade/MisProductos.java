80ab31c7b15535327e8bc5460f2e8e4e744a8c02
Francisco Jose Monrabal Adan <cemi201521@gmail.com>
2018-02-02 14:49:27 +0100

    Ya funciona todo

--- app/src/main/java/com/example/francisco/quicktrade/MisProductos.java
+++ app/src/main/java/com/example/francisco/quicktrade/MisProductos.java
@@ -9,5 +9,6 @@ public class MisProductos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_productos);
+
     }
 }
